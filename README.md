88349bd030e6d644cf4c212be453badfb6b31d1a
Samson Wu <samson959@gmail.com>
2012-02-15 07:22:15 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Overview
+Basically, this is the [Vim-Task](https://github.com/samsonw/vim-task "Vim Task Plugin") port for Sublime Text 2.
+
+##ScreenShot
+TBD
+
+##Installation
+TBD
+
+##Shortcut Key, Key Binding & Customization
+TBD
+
+##File Format & Syntax
+TBD
+
+##Bug & Feedback
+TBD
---
+++ SublimeTask.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import sublime, sublime_plugin
+
+class TaskCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            line_region = self.view.line(region)
+            line = self.view.substr(line_region)
+            processed_line = self.process(line)
+            if processed_line: self.view.replace(edit, line_region, processed_line)
+
+    def process(self, line):
+        # below regex looks nice (works in ruby), but it won't work in python:
+        # p = re.compile('^(\s*)([-✓])(.*)')
+        # http://docs.python.org/howto/unicode
+        p = re.compile(ur'^(\s*)(-|\xe2\x9c\x93)(.*)', re.UNICODE)
+        m = p.match(line.encode(self.view.encoding()))
+        if m:
+            symbol = '✓' if m.group(2) == '-' else '-'
+            result = '%(leading_whitespace)s%(symbol)s%(content)s' % {'leading_whitespace': m.group(1), 'symbol': symbol, 'content': m.group(3)}
+            return result.decode(self.view.encoding())
+        else:
+            return None
